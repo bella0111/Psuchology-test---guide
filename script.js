7ed75920352a2983045dd14abdd4afbdb76ff5df
bella0111 <138566105+bella0111@users.noreply.github.com>
2023-07-05 10:26:41 +0900

    first commit
    첫번째 가이드 코드 입니다

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMainPage() {
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('start-page').style.display = 'none';  
+        document.getElementById('main-page').style.display = 'block';  
+    });
+}
